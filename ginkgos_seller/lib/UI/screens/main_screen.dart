c39974b25d1e442a36d5848b73e4a9dcb9711821
Pulkit0729 <pulkit0729@gmail.com>
2021-07-24 17:04:05 +0530

    Initial Commit
    Initial Commit

---
+++ ginkgos_seller/lib/UI/screens/main_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  static String id = 'main_screen';
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
